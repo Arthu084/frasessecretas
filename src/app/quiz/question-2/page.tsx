192c2e77f52b8a11c8bc262109b2f3549dda76d6
Arthur Macêdo <thurmacedo1@gmail.com>
2025-12-24 15:59:24 +0000

    Desfaça a alteração anteriro e reajuste as imagens

--- src/app/quiz/question-2/page.tsx
+++ src/app/quiz/question-2/page.tsx
@@ -21,17 +21,17 @@ export default function QuizConfidencePage() {
 
       <div className="flex flex-col items-center justify-center text-center mt-8 flex-grow">
         {confidenceImage && (
-          <div className="relative w-[180px] h-[100px] mb-8">
-            <Image
-              src={confidenceImage.imageUrl}
-              alt={confidenceImage.description}
-              width={180}
-              height={100}
-              className="rounded-md object-cover"
-              data-ai-hint={confidenceImage.imageHint}
-            />
-            <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black"></div>
-          </div>
+          <div className="relative w-[200px] h-[120px] mb-8">
+          <Image
+            src={confidenceImage.imageUrl}
+            alt={confidenceImage.description}
+            width={200}
+            height={120}
+            className="rounded-md object-cover"
+            data-ai-hint={confidenceImage.imageHint}
+          />
+          <div className="absolute inset-0 bg-gradient-to-t from-black via-transparent to-transparent"></div>
+        </div>
         )}
 
         <h1 className="text-xl font-bold leading-tight">Você gostaria de se sentir mais segura e confiante ao interagir com qualquer homem?</h1>
--- src/app/quiz/question-3/page.tsx
+++ src/app/quiz/question-3/page.tsx
@@ -22,17 +22,17 @@ export default function QuizDesirePage() {
 
       <div className="flex flex-col items-center justify-center text-center mt-8 flex-grow">
         {desireImage && (
-          <div className="relative w-[180px] h-[100px] mb-8">
-            <Image
-              src={desireImage.imageUrl}
-              alt={desireImage.description}
-              width={180}
-              height={100}
-              className="rounded-md object-cover"
-              data-ai-hint={desireImage.imageHint}
-            />
-             <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black"></div>
-          </div>
+          <div className="relative w-[200px] h-[120px] mb-8">
+          <Image
+            src={desireImage.imageUrl}
+            alt={desireImage.description}
+            width={200}
+            height={120}
+            className="rounded-md object-cover"
+            data-ai-hint={desireImage.imageHint}
+          />
+           <div className="absolute inset-0 bg-gradient-to-t from-black via-transparent to-transparent"></div>
+        </div>
         )}
 
         <h1 className="text-xl font-bold leading-tight">Se você pudesse usar técnicas comprovadas para despertar o desejo de um homem, como isso mudaria sua vida amorosa?</h1>
