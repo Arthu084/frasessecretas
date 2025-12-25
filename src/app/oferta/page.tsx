84bf8e4f8f02200424c602519f96844c45cb6b26
Arthur Macêdo <thurmacedo1@gmail.com>
2025-12-25 21:53:17 +0000

    dIMINUA A H1 PARA CABER EM 2 LINHAS

--- src/app/oferta/page.tsx
+++ src/app/oferta/page.tsx
@@ -63,7 +63,7 @@ export default function OfferPage() {
           </h2>
         </header>
 
-        <section className="bg-zinc-900 py-8 px-4 rounded-xl shadow-lg my-4">
+        <section className="bg-zinc-900 py-8 px-4 rounded-xl shadow-lg my-8">
           <div className="text-center mb-4">
             <h3 className="text-2xl md:text-3xl font-bold">
               Confira o que dizem as mulheres que usaram as{' '}
@@ -127,15 +127,13 @@ export default function OfferPage() {
               maioria das vezes, a única fonte de dopamina do relacionamento é o
               sexo.
             </p>
-            <p>
-              O que você realmente precisa fazer é <span className="font-bold text-red-500">deixá-lo viciado em você</span>, e não apenas no que vocês fazem na cama.
-            </p>
           </div>
         </section>
 
         <section className="my-16">
           <div className="bg-zinc-900 border border-zinc-700 rounded-lg p-6 max-w-md mx-auto">
               <div className="mb-6">
+                 <p className="text-center text-zinc-300 mb-6 text-sm">Baseado em estudos comportamentais, você terá acesso às “Frases Discretas” <span className='font-bold'>que deixam qualquer homem viciado em você!</span></p>
                 <p className="text-center text-zinc-300 mb-6">Suas respostas indicam que você não consegue atrair o homem que deseja por causa desses problemas...</p>
               </div>
               <hr className="border-t border-zinc-700 mb-6"/>
