15a1850b1b4fd9223df1cfca8897c280a2890f04
Arthur Macêdo <thurmacedo1@gmail.com>
2025-12-31 05:49:18 +0000

    Abaixe um pouco.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,7 +23,7 @@ export default function QuizPage() {
       )}
       <div className="absolute inset-0 bg-black/50" />
 
-      <div className="relative z-10 flex flex-col items-center justify-center text-center p-4 mt-20">
+      <div className="relative z-10 flex flex-col items-center justify-center text-center p-4 mt-28">
         <h1 className="text-2xl md:text-3xl font-bold leading-tight">
           Descubra como deixar
           <br />
